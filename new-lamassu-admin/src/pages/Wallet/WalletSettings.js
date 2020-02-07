1aec281c415b3cc8556351fd947d0be593327b55
Luis Félix <5789328+lcsfelix@users.noreply.github.com>
2020-02-07 16:52:20 +0000

    feat: add wallet settings base layout

--- new-lamassu-admin/src/pages/Wallet/WalletSettings.js
+++ new-lamassu-admin/src/pages/Wallet/WalletSettings.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as R from 'ramda'
+import { gql } from 'apollo-boost'
 import { makeStyles } from '@material-ui/core'
+import { useQuery } from '@apollo/react-hooks'
 
 import Title from 'src/components/Title'
-import { Table, THead, Th } from 'src/components/fake-table/Table'
+import {
+  Table,
+  THead,
+  Th,
+  TBody,
+  Tr,
+  Td
+} from 'src/components/fake-table/Table'
+import { Switch } from 'src/components/inputs'
+import { ReactComponent as EditIcon } from 'src/styling/icons/action/edit/enabled.svg'
+import { ReactComponent as DisabledEditIcon } from 'src/styling/icons/action/edit/disabled.svg'
+import commonStyles from 'src/pages/common.styles'
+import { zircon } from 'src/styling/variables'
 
-import logsStyles from '../Logs.styles'
+const localStyles = {
+  disabledDrawing: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    '& > div': {
+      position: 'absolute',
+      backgroundColor: zircon,
+      height: 36,
+      width: 678
+    }
+  }
+}
 
-const localStyles = {}
-
-const styles = R.merge(logsStyles, localStyles)
+const styles = R.merge(commonStyles, localStyles)
 
 const useStyles = makeStyles(styles)
 
@@ -19,7 +43,7 @@ const WALLET_KEY = 'wallet'
 const EXCHANGE_KEY = 'exchange'
 const ZERO_CONF_KEY = 'zeroConf'
 const EDIT_KEY = 'edit'
-const ENABLE_KEY = 'enable'
+const ENABLE_KEY = 'enabled'
 const SIZE_KEY = 'size'
 const TEXT_ALIGN_KEY = 'textAlign'
 
@@ -54,11 +78,57 @@ const columns = {
   }
 }
 
-const OperatorInfo = () => {
+const GET_INFO = gql`
+  {
+    config
+    cryptoCurrencies {
+      code
+      display
+    }
+  }
+`
+
+const schema = {
+  [TICKER_KEY]: '',
+  [WALLET_KEY]: '',
+  [EXCHANGE_KEY]: '',
+  [ZERO_CONF_KEY]: '',
+  [ENABLE_KEY]: false
+}
+
+const WalletSettings = () => {
+  const [cryptoCurrencies, setCryptoCurrencies] = useState(null)
+  const [state, setState] = useState(null)
+
+  useQuery(GET_INFO, {
+    onCompleted: data => {
+      const { cryptoCurrencies, config } = data
+      const wallet = config?.wallet ?? []
+
+      const newState = R.map(crypto => {
+        const el = R.find(R.propEq(CRYPTOCURRENCY_KEY, crypto.code))(wallet)
+        if (!el) return R.assoc(CRYPTOCURRENCY_KEY, crypto.code)(schema)
+        return el
+      })(cryptoCurrencies)
+
+      setState(newState)
+      setCryptoCurrencies(cryptoCurrencies)
+    },
+    onError: error => console.error(error)
+  })
+
   const classes = useStyles()
 
   const getSize = key => columns[key][SIZE_KEY]
   const getTextAlign = key => columns[key][TEXT_ALIGN_KEY]
+  const isSet = crypto =>
+    crypto[TICKER_KEY] &&
+    crypto[WALLET_KEY] &&
+    crypto[EXCHANGE_KEY] &&
+    crypto[ZERO_CONF_KEY]
+  const handleEnable = name => event => console.log(name, event.target.checked)
+
+  if (!state) return null
 
   return (
     <>
@@ -98,10 +168,79 @@ const OperatorInfo = () => {
               Enable
             </Th>
           </THead>
+          <TBody>
+            {state.map((row, idx) => (
+              <Tr key={idx}>
+                <Td
+                  size={getSize(CRYPTOCURRENCY_KEY)}
+                  textAlign={getTextAlign(CRYPTOCURRENCY_KEY)}>
+                  {R.path(['display'])(
+                    R.find(R.propEq('code', row[CRYPTOCURRENCY_KEY]))(
+                      cryptoCurrencies
+                    )
+                  )}
+                </Td>
+                {!isSet(row) && (
+                  <Td
+                    size={
+                      getSize(TICKER_KEY) +
+                      getSize(WALLET_KEY) +
+                      getSize(EXCHANGE_KEY) +
+                      getSize(ZERO_CONF_KEY)
+                    }
+                    textAlign="center"
+                    className={classes.disabledDrawing}>
+                    <div />
+                  </Td>
+                )}
+                {isSet(row) && (
+                  <>
+                    <Td
+                      size={getSize(TICKER_KEY)}
+                      textAlign={getTextAlign(TICKER_KEY)}>
+                      Ticker
+                    </Td>
+                    <Td
+                      size={getSize(WALLET_KEY)}
+                      textAlign={getTextAlign(WALLET_KEY)}>
+                      Wallet
+                    </Td>
+                    <Td
+                      size={getSize(EXCHANGE_KEY)}
+                      textAlign={getTextAlign(EXCHANGE_KEY)}>
+                      Exchange
+                    </Td>
+                    <Td
+                      size={getSize(ZERO_CONF_KEY)}
+                      textAlign={getTextAlign(ZERO_CONF_KEY)}>
+                      Zero Conf
+                    </Td>
+                  </>
+                )}
+                <Td size={getSize(EDIT_KEY)} textAlign={getTextAlign(EDIT_KEY)}>
+                  {!isSet(row) && <DisabledEditIcon />}
+                  {isSet(row) && (
+                    <button className={classes.iconButton}>
+                      <EditIcon />
+                    </button>
+                  )}
+                </Td>
+                <Td
+                  size={getSize(ENABLE_KEY)}
+                  textAlign={getTextAlign(ENABLE_KEY)}>
+                  <Switch
+                    checked={row[ENABLE_KEY]}
+                    onChange={handleEnable(row[CRYPTOCURRENCY_KEY])}
+                    value={row[CRYPTOCURRENCY_KEY]}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </TBody>
         </Table>
       </div>
     </>
   )
 }
 
-export default OperatorInfo
+export default WalletSettings
